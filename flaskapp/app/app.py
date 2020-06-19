92442883bfc354382b2eb331c8c4d78d3d56a207
Sanford Winslow <sanford.winslow@gmail.com>
2020-06-18 18:32:55 -0700

    updating chatbot files

--- flaskapp/app/app.py
+++ flaskapp/app/app.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template, request, jsonify
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 from chatterbot.trainers import ListTrainer
-
+import logging
 
 app = Flask(__name__)
 
-bot = Chatbot('Echo')
+bot = ChatBot('Echo')
 
 trainer = ListTrainer(bot)
 trainer.train(['what is your name?', 'My name is Echo'])
@@ -17,16 +17,18 @@ trainer.train('chatterbot.corpus.english')
 
 @app.route('/')
 def index():
-    greeting = "hello world"
+    greeting = "AD470 Chatbot Project 3"
     return render_template('home.html', greeting = greeting)
 
 @app.route('/predict', methods=['POST'])
 def chatbot_response():
     data = request.get_json()
     print('you posted {0}'.format(data['entry']))
-    return jsonify(response='What?!')
+    logging.debug('data', data)
+    return jsonify(response = 42)
+    
 
 if __name__ == "__main__":
-    app.run(host = '0.0.0.0')
+    app.run(host = '0.0.0.0', port=5000)
     
     
